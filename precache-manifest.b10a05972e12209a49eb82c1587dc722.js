8cc0995c93b120f25024fd08490e795fa37b8e8a
mahfuzalhasan <mahfuzalhasan058@gmail.com>
2021-02-06 00:49:34 -0500

    init repo

---
+++ precache-manifest.b10a05972e12209a49eb82c1587dc722.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa74e04e54e1ad494002",
+    "url": "/css/chunk-vendors.b8c35bda.css"
+  },
+  {
+    "revision": "bbfbad6d8e0e3e9328fb1497491816aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed8bdec6ddfaa63f28fc",
+    "url": "/js/app.9cbb7aff.js"
+  },
+  {
+    "revision": "fa74e04e54e1ad494002",
+    "url": "/js/chunk-vendors.2fd81f6d.js"
+  },
+  {
+    "revision": "3f7c1db4cd0ca4152c44eb2e0dccb005",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
